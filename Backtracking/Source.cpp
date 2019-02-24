17da967b38fda70e030aac56d257440aed8e7f4c
Oltean-Peter Boroka <erboro97@gmail.com>
2019-02-24 19:59:56 +0200

    simple-backtracking

---
+++ Backtracking/Source.cpp
@@ -0,0 +1,13 @@
+#include "Source.h"
+#include "SimpleBacktrack.h"
+#include <iostream> 
+#include <vector> 
+#include <cmath> 
+using namespace std;
+int main()
+{
+	int S = 54, N = 2, P = 3;
+	SimpleBacktrack simpleBacktrack;
+	simpleBacktrack.allPrime(N, S, P);
+	return 0;
+}
